ba269e5ae2e964d768bfaca5662e5a4b34272652
cfmkaifa <cfmkaifa@shdbr.com>
2020-03-13 13:56:19 +0800

    membercenter项目创建,可启动可打包

---
+++ membercenter-provider/src/main/java/org/smartwork/SmartWorkApplication.java
@@ -0,0 +1,25 @@
+package org.smartwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(scanBasePackages={"org.forbes","org.smartwork"})
+@EnableDiscoveryClient
+@EnableSwagger2
+@EnableFeignClients
+public class SmartWorkApplication {
+
+    /***
+     * main方法概述:启动类
+     * @创建人 niehy(Frunk)
+     * @创建时间 2020/3/13
+     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改日期 (请填上修改该文件时的日期)
+     */
+    public static void main(String[] args){
+        SpringApplication.run(SmartWorkApplication.class,args);
+    }
+}
