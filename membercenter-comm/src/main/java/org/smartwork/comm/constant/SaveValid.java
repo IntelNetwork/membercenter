1d8afc593c7c46ef65abb979413ab01ce239881a
cfmkaifa <cfmkaifa@shdbr.com>
2020-03-19 11:12:56 +0800

    公共类删除

--- membercenter-comm/src/main/java/org/smartwork/comm/constant/SaveValid.java
+++
@@ -1,8 +0,0 @@
-package org.smartwork.comm.constant;
-/***
- * SaveValid概要说明：保存验证
- * @author Huanghy
- */
-public interface SaveValid {
-
-}
--- membercenter-comm/src/main/java/org/smartwork/comm/constant/UpdateValid.java
+++
@@ -1,8 +0,0 @@
-package org.smartwork.comm.constant;
-/***
- * UpdateValid概要说明：更新验证
- * @author Huanghy
- */
-public interface UpdateValid {
-
-}
