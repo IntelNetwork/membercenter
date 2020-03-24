9521a7db9a4f587c6062bd2d1b42ad624d013124
li3295552795 <tom.li@shdbr.com>
2020-03-24 10:07:51 +0800

    工作计划接口修改

--- membercenter-biz/src/main/java/org/smartwork/biz/service/impl/ZGWorkPlanServiceImpl.java
+++ membercenter-biz/src/main/java/org/smartwork/biz/service/impl/ZGWorkPlanServiceImpl.java
@@ -2,11 +2,16 @@ package org.smartwork.biz.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.smartwork.biz.service.IZGWorkPlanService;
+import org.smartwork.comm.model.ZGCompanyDto;
+import org.smartwork.comm.model.ZGWorkPlanDto;
+import org.smartwork.dal.entity.ZGCompany;
 import org.smartwork.dal.entity.ZGWorkPlan;
 import org.smartwork.dal.mapper.ZGWorkPlanMapper;
 import org.smartwork.dal.mapper.ext.ZGWorkPlanExtMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -30,4 +35,44 @@ public class ZGWorkPlanServiceImpl extends ServiceImpl<ZGWorkPlanMapper, ZGWorkP
     public List<ZGWorkPlan> selectPlanDay(Date startTime, Date endTime) {
         return zgWorkPlanExtMapper.selectPlanDay(startTime,endTime);
     }
+
+    /***
+     * addWorkPlan方法概述:写工作计划
+     * @param zgWorkPlanDto
+     * @return org.forbes.comm.vo.Result<org.smartwork.comm.model.ZGWorkPlanDto>
+     * @创建人 Tom
+     * @创建时间 2020/3/24 9:49
+     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改日期 (请填上修改该文件时的日期)
+     */
+    @Transactional
+    @Override
+    public void addWorkPlan(ZGWorkPlanDto zgWorkPlanDto) {
+        //添加工作计划信息
+        ZGWorkPlan zgWorkPlan = new ZGWorkPlan();
+        BeanCopier.create(ZGWorkPlanDto.class, ZGWorkPlan.class, false)
+                .copy(zgWorkPlanDto, zgWorkPlan, null);
+        baseMapper.insert(zgWorkPlan);
+    }
+
+    /***
+     * updateWorkPlan方法概述:修改工作计划
+     * @param zgWorkPlanDto
+     * @return org.forbes.comm.vo.Result<org.smartwork.comm.model.ZGWorkPlanDto>
+     * @创建人 Tom
+     * @创建时间 2020/3/24 10:06
+     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改日期 (请填上修改该文件时的日期)
+     */
+    @Transactional
+    @Override
+    public void updateWorkPlan(ZGWorkPlanDto zgWorkPlanDto) {
+        //修改工作计划信息
+        ZGWorkPlan zgWorkPlan = new ZGWorkPlan();
+        BeanCopier.create(ZGWorkPlanDto.class, ZGWorkPlan.class, false)
+                .copy(zgWorkPlanDto, zgWorkPlan, null);
+        baseMapper.updateById(zgWorkPlan);
+    }
+
+
 }
---
+++ membercenter-comm/src/main/java/org/smartwork/comm/model/ZGWorkPlanDto.java
@@ -0,0 +1,224 @@
+package org.smartwork.comm.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Table: fb_zg_work_plan
+ * @author Administrator
+ */
+@Data
+@ApiModel(value = "工作计划参数")
+public class ZGWorkPlanDto implements Serializable {
+
+    private static final long serialVersionUID = 9093780754175897989L;
+
+    /**
+     * id
+     */
+    @ApiModelProperty(value = "id,添加不传值", example = "0")
+    private Long id;
+
+    /**
+     * 人员id
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    user_id
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "人员id",example="0")
+    private Long userId;
+
+    /**
+     * 姓名
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    name
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "姓名",example="")
+    private String name;
+
+    /**
+     * 标题
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    title
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "标题",example="")
+    private String title;
+
+    /**
+     * 工作内容
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    content
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "工作内容",example="")
+    private String content;
+
+    /**
+     * 开始时间
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    start_time
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "开始时间",example="")
+    private Date startTime;
+
+    /**
+     * 结束时间
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    end_time
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "结束时间",example="")
+    private Date endTime;
+
+    /**
+     * Table:     fb_zg_work_plan
+     * Column:    status
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "",example="")
+    private String status;
+
+    /**
+     * 实际开始时间
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    actual_start_time
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "实际开始时间",example="")
+    private Date actualStartTime;
+
+    /**
+     * 实际结束时间
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    actual_end_time
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "实际结束时间",example="")
+    private Date actualEndTime;
+
+    /**
+     * 进度
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    ratio
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "进度",example="0.00")
+    private BigDecimal ratio;
+
+    /**
+     * 权重点
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    weight
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "权重点",example="")
+    private Byte weight;
+
+    /**
+     * 绩效点
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    performance
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "绩效点",example="")
+    private Byte performance;
+
+    /**
+     * 实际绩效点
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    actual_pert
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "实际绩效点",example="")
+    private Byte actualPert;
+
+    /**
+     * 绩效评估
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    pert_assess
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "绩效评估",example="")
+    private Byte pertAssess;
+
+    /**
+     * 实际绩效评估
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    actual_pert_assess
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "实际绩效评估",example="")
+    private Byte actualPertAssess;
+
+    /**
+     * 管理权重评估
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    manager_weight
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "管理权重评估",example="")
+    private Byte managerWeight;
+
+    /**
+     * 管理绩效评估
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    manag_pert_assess
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "管理绩效评估",example="")
+    private Byte managPertAssess;
+
+    /**
+     * 评估人ID
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    assess_id
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "评估人ID",example="0")
+    private Long assessId;
+
+    /**
+     * 评估人
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    assess_name
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "评估人",example="")
+    private String assessName;
+
+    /**
+     * 说明
+     *
+     * Table:     fb_zg_work_plan
+     * Column:    des
+     * Nullable:  true
+     */
+    @ApiModelProperty(value = "说明",example="")
+    private String des;
+}
