353a3a3632a077b4b7775103b5713e5bddf3e24c
cfmkaifa <cfmkaifa@shdbr.com>
2020-03-26 18:38:48 +0800

    创建项目

--- membercenter-api/src/main/java/org/smartwork/provider/ZGProjectApiProvider.java
+++ membercenter-api/src/main/java/org/smartwork/provider/ZGProjectApiProvider.java
@@ -13,12 +13,16 @@ import org.forbes.comm.model.BasePageDto;
 import org.forbes.comm.model.SysUser;
 import org.forbes.comm.utils.ConvertUtils;
 import org.forbes.comm.vo.Result;
-import org.smartwork.biz.service.IZGProjectService;
+import org.smartwork.biz.service.*;
+import org.smartwork.comm.enums.CmAdminFlagEnum;
 import org.smartwork.comm.enums.MemberBizResultEnum;
 import org.smartwork.comm.enums.ProjectStateEnum;
+import org.smartwork.comm.enums.ProjectTypeEnum;
 import org.smartwork.comm.model.ProjectPageDto;
 import org.smartwork.comm.model.ZGProjectDto;
+import org.smartwork.dal.entity.ZGCmRelUser;
 import org.smartwork.dal.entity.ZGProject;
+import org.smartwork.dal.entity.ZGTeamRelUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 概述:项目操作
  * @创建人 niehy(Frunk)
  * @创建时间 2020/3/24
- * @修改人 (修改了该文件，请填上修改人的名字)
+ * @修改人 (修改了该文件 ， 请填上修改人的名字)
  * @修改日期 (请填上修改该文件时的日期)
  */
 @RestController
@@ -41,6 +45,14 @@ public class ZGProjectApiProvider {
 
     @Autowired
     IZGProjectService projectService;
+    @Autowired
+    IZGTeamService teamService;
+    @Autowired
+    IZGTeamRelUserService teamRelUserService;
+    @Autowired
+    IZGCompanyService companyService;
+    @Autowired
+    IZGCmRelUserService cmRelUserService;
 
 
     /***
@@ -48,7 +60,7 @@ public class ZGProjectApiProvider {
      * @param pageDto
      * @创建人 niehy(Frunk)
      * @创建时间 2020/3/24
-     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改人 (修改了该文件 ， 请填上修改人的名字)
      * @修改日期 (请填上修改该文件时的日期)
      */
     @RequestMapping(value = "/create-project", method = RequestMethod.GET)
@@ -79,7 +91,7 @@ public class ZGProjectApiProvider {
      * @param projectDto
      * @创建人 niehy(Frunk)
      * @创建时间 2020/3/24
-     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改人 (修改了该文件 ， 请填上修改人的名字)
      * @修改日期 (请填上修改该文件时的日期)
      */
     @RequestMapping(value = "/create-project", method = RequestMethod.POST)
@@ -91,8 +103,40 @@ public class ZGProjectApiProvider {
             result.setMessage(MemberBizResultEnum.ENTITY_EMPTY.getBizMessage());
             return result;
         }
+        //只有任务类型为0 团队类型 和 1 公司类型 的时候才能创建项目
+        if (!projectDto.getDataType().equals(ProjectTypeEnum.COMPANY_PRO.getCode())
+                || !projectDto.getDataType().equals(ProjectTypeEnum.TEAM_PRO.getCode())) {
+            result.setBizCode(MemberBizResultEnum.DATA_TYPE_NO_EXIST.getBizCode());
+            result.setMessage(MemberBizResultEnum.DATA_TYPE_NO_EXIST.getBizMessage());
+            return result;
+        }
         SysUser user = UserContext.getSysUser();
-        //判断当前操作人是否为团队/公司的管理员
+        //判断项目数据类型是否为团队类型
+        if (projectDto.getDataType().equals(ProjectTypeEnum.TEAM_PRO.getCode())) {
+            //判断判断当前操作人是否为团队的管理员
+            QueryWrapper<ZGTeamRelUser> query = new QueryWrapper<>();
+            query.eq("user_id", user.getId());
+            query.eq("team_id", projectDto.getDataId());
+            ZGTeamRelUser teamRelUser = teamRelUserService.getOne(query);
+            if (!teamRelUser.getAdminFlag().equals(CmAdminFlagEnum.ORDINARY.getCode())) {
+                result.setBizCode(MemberBizResultEnum.NO_PERMISSION_TO_MODIFY.getBizCode());
+                result.setMessage(MemberBizResultEnum.NO_PERMISSION_TO_MODIFY.getBizMessage());
+                return result;
+            }
+        }
+        //判断项目数据类型是否为公司类型
+        if (projectDto.getDataType().equals(ProjectTypeEnum.COMPANY_PRO.getCode())) {
+            //判断判断当前操作人是公司类型的管理员
+            QueryWrapper<ZGCmRelUser> query = new QueryWrapper<>();
+            query.eq("user_id", user.getId());
+            query.eq("cm_id", projectDto.getDataId());
+            ZGCmRelUser cmRelUser = cmRelUserService.getOne(query);
+            if (!cmRelUser.getAdminFlag().equals(CmAdminFlagEnum.ORDINARY.getCode())) {
+                result.setBizCode(MemberBizResultEnum.NO_PERMISSION_TO_MODIFY.getBizCode());
+                result.setMessage(MemberBizResultEnum.NO_PERMISSION_TO_MODIFY.getBizMessage());
+                return result;
+            }
+        }
         projectDto.setStatus(ProjectStateEnum.NOT_YET_BEGUN.getCode());
         projectService.createPro(projectDto);
         result.setResult(projectDto);
@@ -100,13 +144,12 @@ public class ZGProjectApiProvider {
     }
 
 
-
     /***
      * 方法概述:编辑项目
      * @param projectDto
      * @创建人 niehy(Frunk)
      * @创建时间 2020/3/24
-     * @修改人 (修改了该文件，请填上修改人的名字)
+     * @修改人 (修改了该文件 ， 请填上修改人的名字)
      * @修改日期 (请填上修改该文件时的日期)
      */
     @RequestMapping(value = "/modify-project", method = RequestMethod.POST)
